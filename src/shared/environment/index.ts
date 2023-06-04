40cff54825452020ef591f911420b0b33c503705
Ribeiro <42475620+alexfariakof@users.noreply.github.com>
2023-06-04 09:35:58 -0300

    removendo pasta build

--- src/shared/environment/index.ts
+++ src/shared/environment/index.ts
@@ -2,5 +2,5 @@
 export const environment = {
     LIMITE_DE_LINHAS: 10,
     INPUT_DE_BUSCA : '',
-    URL_BASE: 'http://api-despesas-pessoais-aspnetcore.bwg2czahbvgefufr.eastus.azurecontainer.io/api',
+    URL_BASE: 'https://localhost:44377/api',
 };
