810e00c550def6a77421460fba742fdb48e7fa0f
Ribeiro <42475620+alexfariakof@users.noreply.github.com>
2023-06-14 22:22:36 -0300

    Atualizações design responsivo first mobile  para corrigir gráfico de acordo com a orientação  da tela

--- src/shared/components/bar-charts/BarCharts.tsx
+++ src/shared/components/bar-charts/BarCharts.tsx
@@ -66,7 +66,7 @@ const labels = [
   'Dezembro',
 ];
 
-const data: ChartData = {
+const charData: ChartData = {
   labels,
   datasets: [
     {
@@ -87,25 +87,84 @@ const data: ChartData = {
 export const BarCharts: React.FC = () => {
   const [chartHeight, setChartHeight] = useState(0);
   const smDown = useMediaQuery((theme: Theme) => theme.breakpoints.down('sm'));
+  const [options, setOptions] = useState(smDown ? optionsRight : optionsTop);
+  const [data, setData] = useState<any>(charData);
 
   useEffect(() => {
-    const handleResize = () => {
-      setChartHeight(window.innerHeight * 0.7); // Define the chart's height as 80% of the window's height
-    };
+      const handleResize = () => {
+          if (smDown) {
+              setChartHeight(window.innerHeight);
+          }
+          else {
+              setChartHeight(window.innerHeight * 0.7);
+          }
+
+      };
 
     window.addEventListener('resize', handleResize);
-    handleResize(); // Set the chart's height when the component mounts
+    handleResize(); 
+    
+    setData({
+        labels,
+        datasets: [
+            {
+                label: 'Despesas',
+                data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
+                borderColor: 'rgb(255, 99, 132)',
+                backgroundColor: 'rgba(255, 99, 132, 0.5)',
+            },
+            {
+                label: 'Receitas',
+                data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
+                borderColor: 'rgb(53, 162, 235)',
+                backgroundColor: 'rgba(53, 162, 235, 0.5)',
+            },
+        ],
+    });
 
     return () => {
       window.removeEventListener('resize', handleResize);
     };
-  }, []);
+  }, [smDown]);
+
+  useEffect(() => {
+    const handleOrientationChange = () => {
+        if (smDown) {
+            setOptions(optionsTop);
+            setChartHeight(window.innerHeight);
+          } else {
+            setOptions(optionsRight);
+            setChartHeight(window.innerHeight * 0.7);
+          }
+          setData({
+            labels,
+            datasets: [
+                {
+                    label: 'Despesas',
+                    data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
+                    borderColor: 'rgb(255, 99, 132)',
+                    backgroundColor: 'rgba(255, 99, 132, 0.5)',
+                },
+                {
+                    label: 'Receitas',
+                    data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
+                    borderColor: 'rgb(53, 162, 235)',
+                    backgroundColor: 'rgba(53, 162, 235, 0.5)',
+                },
+            ],
+        });
+    };
+    window.addEventListener('orientationchange', handleOrientationChange);
+
+
+    return () => {
+        window.removeEventListener('orientationchange', handleOrientationChange);
+      };  
+  }, [smDown, options]);
 
   return (
-    <Box sx={{ height: smDown ? chartHeight : '75vh', width: '100%' }}>
-      <Bar data={data} options={smDown ? optionsRight : optionsTop} />
+    <Box sx={{ height: smDown ? chartHeight : '70vh', width: '100%' }}>
+      <Bar data={data} options={options} />
     </Box>
   );
 };
-
- 
