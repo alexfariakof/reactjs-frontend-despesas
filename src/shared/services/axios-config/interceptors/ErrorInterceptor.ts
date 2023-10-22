2e9cff7aaebc791ff05cbdee5b484be282caeabf
Ribeiro <42475620+alexfariakof@users.noreply.github.com>
2023-10-22 18:04:08 -0300

    Update ErrorInterceptor.ts

--- src/shared/services/axios-config/interceptors/ErrorInterceptor.ts
+++ src/shared/services/axios-config/interceptors/ErrorInterceptor.ts
@@ -1,7 +1,7 @@
 import {AxiosError}  from 'axios';
 
 export const errorInterceptor = (error: AxiosError) => {    
-    var data:any;
+    //var data:any;
     
     if (error.response?.status === 400){ // Este erro é um erro de BadRequest e deverá ser tratado dentro das requisições 
         //return Promise.reject(false);
