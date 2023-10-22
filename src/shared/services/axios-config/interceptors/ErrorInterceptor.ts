0d041b45d6475a4a5a3d94dc23bbec0d17043ad5
Ribeiro <42475620+alexfariakof@users.noreply.github.com>
2023-10-22 17:31:57 -0300

    'Merge tag hotfix/fixing-erros-in-load-imagem-perfil-usuario'

--- src/shared/services/axios-config/interceptors/ErrorInterceptor.ts
+++ src/shared/services/axios-config/interceptors/ErrorInterceptor.ts
@@ -3,19 +3,18 @@ import {AxiosError}  from 'axios';
 export const errorInterceptor = (error: AxiosError) => {    
     var data:any;
     
-    if (error.response?.status === 400){        
-        data = error.response.data; 
-        console.log(data.message);
+    if (error.response?.status === 400){ // Este erro é um erro de BadRequest e deverá ser tratado dentro das requisições 
+        //return Promise.reject(false);
     } else  if (error.response?.status ===  401){
-        localStorage.clear();
-        data = error.response.data; 
-        console.log(data.message);
-    } else if (error.response?.status ===  415){
-        data = error.response.data; 
-        console.log(data.message);
+        localStorage.clear();        
+        // eslint-disable-next-line no-restricted-globals
+        location.reload();
+        console.log(error);
     } else if (error.message === 'Network Error'){
-        console.log('Erro de conexão!');
-        localStorage.clear();
+        localStorage.clear();        
+        // eslint-disable-next-line no-restricted-globals
+        location.reload();
+        console.log('Erro de conexão!');        
     }
     return Promise.reject(error);
 };
